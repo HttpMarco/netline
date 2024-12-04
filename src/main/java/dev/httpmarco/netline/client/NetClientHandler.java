3e3766aac7c2c090b62c6a882f1e6d5b66ea50fe
HttpMarco <mirco.lindenau@gmx.de>
2024-12-04 10:23:12 +0100

    Use right channel id

--- src/main/java/dev/httpmarco/netline/client/NetClientHandler.java
+++ src/main/java/dev/httpmarco/netline/client/NetClientHandler.java
@@ -5,7 +5,10 @@ import dev.httpmarco.netline.client.common.AbstractNetClient;
 import dev.httpmarco.netline.common.AbstractNetCompHandler;
 import dev.httpmarco.netline.request.RequestScheme;
 import io.netty5.channel.Channel;
+import lombok.extern.log4j.Log4j2;
+import org.jetbrains.annotations.NotNull;
 
+@Log4j2
 public final class NetClientHandler extends AbstractNetCompHandler {
 
     private final AbstractNetClient client;
@@ -21,12 +24,16 @@ public final class NetClientHandler extends AbstractNetCompHandler {
     }
 
     @Override
-    public void netChannelOpen(NetChannel channel) {
-
-        //todo
-        channel.updateId("LOCAL_ETST");
+    public void netChannelOpen(@NotNull NetChannel channel) {
         client.channel(channel);
 
+        if(client.bootFuture() == null) {
+            log.warn("Client boot future is null. So we can't proceed with the channel open event.");
+            channel.close();
+            return;
+        }
+
+        channel.updateId(client.config().id);
         client.request(RequestScheme.CLIENT_AUTH).send(client.config().id).whenComplete((result, throwable) -> {
             if (result) {
                 client.available(true);
