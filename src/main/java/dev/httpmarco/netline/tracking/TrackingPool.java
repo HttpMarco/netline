44560a0946511a783663a67a9ac5f3f01ecd6a91
HttpMarco <mirco.lindenau@gmx.de>
2024-12-04 01:34:41 +0100

    Work on trackings

--- src/main/java/dev/httpmarco/netline/tracking/TrackingPool.java
+++ src/main/java/dev/httpmarco/netline/tracking/TrackingPool.java
@@ -1,13 +1,79 @@
 package dev.httpmarco.netline.tracking;
 
+import dev.httpmarco.netline.request.RequestChannelResponder;
+import dev.httpmarco.netline.request.RequestResponder;
+import dev.httpmarco.netline.request.RequestScheme;
+import org.jetbrains.annotations.NotNull;
+
 import java.util.UUID;
 
 public interface TrackingPool {
 
+    /**
+     * Track a specific tracking type with a tracker
+     * @param tracking the tracking type
+     * @param tracker the tracker
+     * @return the tracking id
+     * @param <A> the tracking type
+     */
     <A extends Tracking> UUID put(Class<A> tracking, ChannelTracker<A> tracker);
 
+    /**
+     * Clear all tracking and responders
+     */
     void clear();
 
+    /**
+     * Remove a current racking by id
+     *
+     * @param trackingId the tracking id
+     */
     void remove(UUID trackingId);
 
+    /**
+     * Register a responder for a specific request scheme
+     *
+     * @param scheme    the request scheme
+     * @param responder the responder
+     * @param <R>       the request type
+     * @param <A>       the response type
+     */
+    <R, A> void responderOf(RequestScheme<R, A> scheme, RequestChannelResponder<R, A> responder);
+
+    /**
+     * Register a responder for a specific request scheme, without the channel
+     *
+     * @param scheme    the request scheme
+     * @param responder the responder
+     * @param <R>       the request type
+     * @param <A>       the response type
+     */
+    default <R, A> void responderOf(RequestScheme<R, A> scheme, RequestResponder<R, A> responder) {
+        responderOf(scheme, (request, channel) -> responder.respond(request));
+    }
+
+    /**
+     * Check if a responder is present for a specific request scheme
+     *
+     * @param requestID the request id
+     * @return true if a responder is present
+     */
+    boolean responderPresent(String requestID);
+
+    /**
+     * Check if a responder is present for a specific request scheme
+     *
+     * @param schema the request scheme
+     * @return true if a responder is present
+     */
+    default boolean responderPresent(@NotNull RequestScheme<?, ?> schema) {
+        return responderPresent(schema.id());
+    }
+
+    /**
+     * Respond to a specific request id
+     * @param requestID the request id
+     * @return the responder
+     */
+    RequestChannelResponder<?, ?> responder(String requestID);
 }
--- src/main/java/dev/httpmarco/netline/tracking/TrackingProvider.java
+++ src/main/java/dev/httpmarco/netline/tracking/TrackingProvider.java
@@ -1,6 +1,12 @@
 package dev.httpmarco.netline.tracking;
 
 import dev.httpmarco.netline.channel.NetChannel;
+import dev.httpmarco.netline.packet.Packet;
+import dev.httpmarco.netline.request.NetRequest;
+import dev.httpmarco.netline.request.RequestChannelResponder;
+import dev.httpmarco.netline.request.RequestResponder;
+import dev.httpmarco.netline.request.RequestScheme;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
@@ -33,7 +39,6 @@ public interface TrackingProvider {
      */
     void call(NetChannel channel, Tracking tracking);
 
-
     /**
      * Untracked a specific tracking id
      * @param trackingId the tracking id
@@ -50,5 +55,40 @@ public interface TrackingProvider {
      * @return the tracking pool
      */
     TrackingPool trackingPool();
+
+    /**
+     * Wait for a specific request id
+     * @param id the request id
+     * @param result the result function
+     * @param <T> the request type
+     */
+    default <R, T> void waitFor(RequestScheme<R, T> id, RequestResponder<R, T> result) {
+        waitFor(id, (it, channel) -> result.respond(it));
+    }
+
+    /**
+     * Wait for a specific request id
+     * @param id the request id
+     * @param result the result function
+     * @param <R> the result type
+     * @param <T> the request type
+     */
+    <R, T> void waitFor(RequestScheme<R, T> id, RequestChannelResponder<R, T> result);
+
+    /**
+     * Request a specific request id
+     * @param id the request id
+     * @return the request
+     * @param <T> the request type
+     */
+    <T> NetRequest<T> request(@NotNull RequestScheme<?, T> id);
+
+    /**
+     * Call a request with a response
+     * @param channel the channel
+     * @param requestId the request id
+     * @param response the response
+     */
+    void callRequest(NetChannel channel, UUID requestId, Packet response);
 
 }
