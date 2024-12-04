ce2664611669abf2b8a1a6884093c724225443f1
HttpMarco <mirco.lindenau@gmx.de>
2024-12-04 01:09:16 +0100

    Remove duplicated request interface

--- src/main/java/dev/httpmarco/netline/request/NetRequestProvider.java
+++
@@ -1,9 +0,0 @@
-package dev.httpmarco.netline.request;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface NetRequestProvider {
-
-    <T> NetRequest<T> request(@NotNull RequestScheme<?, T> id);
-
-}
