86f0b626afbae3a551b112ef489385cc54b39766
HttpMarco <mirco.lindenau@gmx.de>
2024-12-06 23:52:51 +0100

    Add cluster test

---
+++ src/test/java/dev/httpmarco/netline/tests/ClusterTest.java
@@ -0,0 +1,16 @@
+package dev.httpmarco.netline.tests;
+
+import org.junit.jupiter.api.*;
+
+@Nested
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DisplayName("5 - Node test")
+public final class ClusterTest {
+
+    @Test
+    @Order(1)
+    @DisplayName("5.1 Boot first node")
+    public void handleClusterBoot() {
+
+    }
+}
