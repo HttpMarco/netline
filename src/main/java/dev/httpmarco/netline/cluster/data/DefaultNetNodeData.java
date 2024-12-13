25accc43b28d8f58b2e90fa170558c9f2142e96e
Mirco <mirco@bytemc.de>
2024-12-13 14:30:11 +0100

    Add login time for the head node data

--- src/main/java/dev/httpmarco/netline/cluster/data/DefaultNetNodeData.java
+++ src/main/java/dev/httpmarco/netline/cluster/data/DefaultNetNodeData.java
@@ -1,6 +1,13 @@
 package dev.httpmarco.netline.cluster.data;
 
 import dev.httpmarco.netline.cluster.NetNodeData;
+import lombok.Getter;
+import lombok.experimental.Accessors;
 
+@Getter
+@Accessors(fluent = true)
 public final class DefaultNetNodeData implements NetNodeData {
+
+    private final long initializationTime = System.currentTimeMillis();
+
 }
